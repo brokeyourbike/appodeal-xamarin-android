11b1c90ad6f338d41eff01be7677849c94c19b1d
Ivan Stasiuk <brokeyourbike@gmail.com>
2025-09-17 16:16:01 +0000

    feat: resources

---
+++ Jars/Resource.designer.cs
@@ -0,0 +1,17 @@
+namespace AppodealXamarinPlugin
+{
+    public partial class Resource
+    {
+        public partial class Drawable
+        {
+            public const int apd_ad_choice_preview = 0x7f01000e;
+            public const int apd_icon_preview = 0x7f01000f;
+            public const int apd_media_preview = 0x7f010010;
+        }
+
+        public partial class Style
+        {
+            public const int Theme_AppodealStackConsentActivity = 0x7f010015;
+        }
+    }
+}
